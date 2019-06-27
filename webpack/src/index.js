b11417d50c69d8e2b1d50e49d2c5e197a6a93d5f
hongwuzhao <hongwuzhao@tencent.com>
2019-06-27 16:48:01 +0800

    增加pdfjs支持

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,2 +1,31 @@
-import App from "./App";
-import style from "./main.css";
+// import App from "./App";
+// import style from "./main.css";
+// import PDFjs from 'pdfjs-dist';
+
+var pdfjsLib = require('pdfjs-dist');
+var pdfPath = '../src/hello.pdf';
+
+// Setting worker path to worker bundle.
+// pdfjsLib.GlobalWorkerOptions.workerSrc =
+//   '../../build/webpack/pdf.worker.bundle.js';
+
+// Loading a document.
+var loadingTask = pdfjsLib.getDocument(pdfPath);
+loadingTask.promise.then(function (pdfDocument) {
+  // Request a first page
+  return pdfDocument.getPage(1).then(function (pdfPage) {
+    // Display page on the existing canvas with 100% scale.
+    var viewport = pdfPage.getViewport(1.0);
+    var canvas = document.getElementById('theCanvas');
+    canvas.width = viewport.width;
+    canvas.height = viewport.height;
+    var ctx = canvas.getContext('2d');
+    var renderTask = pdfPage.render({
+      canvasContext: ctx,
+      viewport: viewport
+    });
+    return renderTask.promise;
+  });
+}).catch(function (reason) {
+  console.error('Error: ' + reason);
+});
